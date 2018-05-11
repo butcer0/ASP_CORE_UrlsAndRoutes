c5e17cce098921ee76971d09da5dcafaf45fa016
Erik Butcher <butcer0@gmail.com>
2018-05-11 14:54:55 -0400

    Created basic routing with Result model and controllers

---
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ViewResult Index() => View("Result",
+            new Result
+            {
+                Controller = nameof(CustomerController),
+                Action = nameof(Index)
+            });
+
+        public ViewResult List() => View("Result",
+            new Result
+            {
+                Controller = nameof(CustomerController),
+                Action = nameof(List)
+            });
+    }
+}
