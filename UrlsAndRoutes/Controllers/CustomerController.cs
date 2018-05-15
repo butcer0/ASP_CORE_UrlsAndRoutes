fd7803d2af87c3ebb6a088a90f4cd731e91dc4b8
Erik Butcher <butcer0@gmail.com>
2018-05-15 16:41:06 -0400

    Completed basic routing with custom attribute constraints

--- UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -7,8 +7,26 @@ using UrlsAndRoutes.Models;
 
 namespace UrlsAndRoutes.Controllers
 {
+    //Erik - 5/15/2018 Mixes static and variabble route segments
+
+    #region Depricated - 5/15/2018 Introduced Custom Constraint
+    /*
+    [Route("app/[controller]/actions/[action]/{id?}")]
+    */
+    #endregion
+    [Route("app/[controller]/actions/[action]/{id:weekday?}")]
     public class CustomerController : Controller
     {
+        #region Depricated - 5/15/2018 Using direct Route isn't usually very useful, only route you can get to method and replaces and prevents previous path
+        /*
+        [Route("myroute")]
+        */
+        #endregion
+        #region Depricated - 5/15/2018 Use complex attribute routing
+        /*
+        [Route("[controller]/MyAction")]
+        */
+        #endregion        
         public ViewResult Index() => View("Result",
             new Result
             {
--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.Extensions.DependencyInjection;
+using UrlsAndRoutes.Infrastructure;
 
 namespace UrlsAndRoutes
 {
@@ -15,6 +18,7 @@ namespace UrlsAndRoutes
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RouteOptions>(options => options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint)));
             services.AddMvc();
         }
 
@@ -24,99 +28,172 @@ namespace UrlsAndRoutes
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvc(routes =>
-            {
-                //Erik - 5/11/2018 Inline-constraint to only match first segment when begins with 'H'. Default applied before constraint evaluated
-                routes.MapRoute(
-                   name: "MyRoute",
-                   template: "{controller:regex(^H.*)=Home}/{action=Index}/{id?}/{*catchall}"
-                   );
+            app.UseMvcWithDefaultRoute();
+            #region Depricated - 5/15/2018 Converted back to Default Routing for Attribute Routing
+            /*
+    //        app.UseMvc(routes =>
+    //    {
+    //        routes.MapRoute(name: "MyRoute",
+    //            template: "{controller=Home}/{action=Index}/{id:weekday?}");
 
-                //Erik - 5/11/2018 Segment-Level constraints -> defaults required to be written as separate arguments along with constraints
-                #region Depricated - 5/11/2018 Use Inline-Constraints
+    //        #region Depricated - 5/15/2018 Converted to In-Line Syntax
+    //        /*
+    //        routes.MapRoute(name: "MyRoute",
+    //          template: "{controller}/{action}/{id?}",
+    //          defaults: new { controller = "Home", action = "Index" },
+    //          constraints: new
+    //          {
+    //              id = new WeekDayConstraint()
+    //          });
 
-                //routes.MapRoute(
-                //               name: "MyRoute",
-                //               template: "{controller}/{action}/{id?}/{*catchall}",
-                //               defaults: new { controller = "Home", action = "Index" },
-                //               constraints: new
-                //               {
-                //                   controller = new Microsoft.AspNetCore.Routing.Constraints.RegexRouteConstraint("^H.*")
-                //               });
+    //        */
+    //        #endregion
 
-                #endregion
+    //        //Erik - 5/15/2018 Composite Constraint Syntax requires Using CompositeRouteConstraint Class
+    //        #region Depricated - 5/15/2018 Introduced Custom Configuration
+    //        /*
+    //          routes.MapRoute(name: "MyRoute",
+    //          template: "{controller}/{action}/{id?}",
+    //          defaults: new { controller = "Home", action = "Index" },
+    //          constraints: new
+    //          {
+    //              id = new CompositeRouteConstraint(
+    //                  new IRouteConstraint[]
+    //                  {
+    //                      new AlphaRouteConstraint(),
+    //                      new MinLengthRouteConstraint(6)
+    //                  })
+    //          });
+    //        */
+    //        #endregion
 
 
-                //Erik - 5/11/2018 catchall (either in <RouteData> or method parameter) will pick up anything passed 3rd segment together
-                #region Depricated - 5/11/2018 Introduced Constraints
-                /*
-				           
-                // ex: .../Customer/List/All/Delete/Perm -> id = "All", catchall = "Delete/Perm"
-                routes.MapRoute(
-                   name: "MyRoute",
-                   template: "{controller=Home}/{action=Index}/{id?}/{*catchall}"
-                   );
-    
-				*/
-                #endregion
+    //        #region Depricated - 5/15/2018 Converted to Composite Syntax
+    //        /*
+    //         routes.MapRoute(name: "MyRoute",
+    //           template: "{controller=Home}/{action=Index}/{id:alpha:minlength(6)?}");
+    //        */
+    //        #endregion
 
-                //Erik - 5/11/2018 id can be picked up by controller method either in <RouteData> or as parameter named 'id'
-                #region Depricated - 5/11/2018 Introduced variable length catchall
-                /*
-				
-                routes.MapRoute(
-                   name: "MyRoute",
-                   template: "{controller=Home}/{action=Index}/{id?}"
-                   );
-				*/
-                #endregion
 
-                #region Depricated - 5/11/2018 Introduced Optional Custom URL Segment
-                /*
-				 routes.MapRoute(
-                    name: "MyRoute",
-                    template: "{controller=Home}/{action=Index}/{id=DefaultId}"
-                    );
-				*/
-                #endregion
+    //        #region Depricated - 5/15/2018 Introduced Chained Constraints
+    //        /*
+    //         routes.MapRoute(name: "MyRoute",
+    //            template: "{controller=Home}/{action=Index}/{id:range(10,20)?}");
+    //        */
+    //        #endregion
 
-                #region Depricated - 5/11/2018 Introduced Custom Segment Variables
-                /*
-				 routes.MapRoute(
-                    name: "ShopSchema2",
-                    template: "Shop/OldAction",
-                    defaults: new { controller = "Home", action = "Index" }
-                    );
 
-                routes.MapRoute(
-                    name: "ShopSchema",
-                    template: "Shop/{action}",
-                    defaults: new { controller = "Home" });
+    //        //Erik - 5/15/2018 Only match when controller begins with H or Defaulted and action equals Index or About
+    //        #region Depricated - 5/15/2018 Introduced Type and Value Constraint
+    //        /*
+    //         routes.MapRoute(
+    //       name: "MyRoute",
+    //       template: "{controller:regex(^H.*)=Home}/{action:regex(^Index$|^About$)=Index}/{id?}"
+    //           );
+    //        */
+    //        #endregion
 
-                routes.MapRoute("", "X{controller}/{action}");
 
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}"
-                    );
+    //        //Erik - 5/11/2018 Inline-constraint to only match first segment when begins with 'H'. Default applied before constraint evaluated
+    //        #region Depricated - 5/15/2018 Constrain Regex to Only Match Specific Values
+    //        /*
+    //         routes.MapRoute(
+    //           name: "MyRoute",
+    //           template: "{controller:regex(^H.*)=Home}/{action=Index}/{id?}/{*catchall}"
+    //           );
+    //        */
+    //        #endregion
 
-                routes.MapRoute(
-                    name: "",
-                    template: "Public/{controller=Home}/{action=Index}"
-                    );
-				*/
-                #endregion
 
-                #region Depricated - 5/11/2018 Use Inline Defaulting
-                /*
-				 routes.MapRoute(
-                    name: "default", 
-                    template: "{controller}/{action}",
-                    defaults: new { action = "Index" });
-				*/
-                #endregion
+    //        //Erik - 5/11/2018 Segment-Level constraints -> defaults required to be written as separate arguments along with constraints
+    //        #region Depricated - 5/11/2018 Use Inline-Constraints
 
-            });
+    //        //routes.MapRoute(
+    //        //               name: "MyRoute",
+    //        //               template: "{controller}/{action}/{id?}/{*catchall}",
+    //        //               defaults: new { controller = "Home", action = "Index" },
+    //        //               constraints: new
+    //        //               {
+    //        //                   controller = new Microsoft.AspNetCore.Routing.Constraints.RegexRouteConstraint("^H.*")
+    //        //               });
+
+    //        #endregion
+
+
+    //        //Erik - 5/11/2018 catchall (either in <RouteData> or method parameter) will pick up anything passed 3rd segment together
+    //        #region Depricated - 5/11/2018 Introduced Constraints
+    //        /*
+
+    //        // ex: .../Customer/List/All/Delete/Perm -> id = "All", catchall = "Delete/Perm"
+    //        routes.MapRoute(
+    //           name: "MyRoute",
+    //           template: "{controller=Home}/{action=Index}/{id?}/{*catchall}"
+    //           );
+
+    //        */
+    //        #endregion
+
+    //        //Erik - 5/11/2018 id can be picked up by controller method either in <RouteData> or as parameter named 'id'
+    //        #region Depricated - 5/11/2018 Introduced variable length catchall
+    //        /*
+
+    //        routes.MapRoute(
+    //           name: "MyRoute",
+    //           template: "{controller=Home}/{action=Index}/{id?}"
+    //           );
+    //        */
+    //        #endregion
+
+    //        #region Depricated - 5/11/2018 Introduced Optional Custom URL Segment
+    //        /*
+    //         routes.MapRoute(
+    //            name: "MyRoute",
+    //            template: "{controller=Home}/{action=Index}/{id=DefaultId}"
+    //            );
+    //        */
+    //        #endregion
+
+    //        #region Depricated - 5/11/2018 Introduced Custom Segment Variables
+    //        /*
+    //         routes.MapRoute(
+    //            name: "ShopSchema2",
+    //            template: "Shop/OldAction",
+    //            defaults: new { controller = "Home", action = "Index" }
+    //            );
+
+    //        routes.MapRoute(
+    //            name: "ShopSchema",
+    //            template: "Shop/{action}",
+    //            defaults: new { controller = "Home" });
+
+    //        routes.MapRoute("", "X{controller}/{action}");
+
+    //        routes.MapRoute(
+    //            name: "default",
+    //            template: "{controller=Home}/{action=Index}"
+    //            );
+
+    //        routes.MapRoute(
+    //            name: "",
+    //            template: "Public/{controller=Home}/{action=Index}"
+    //            );
+    //        */
+    //        #endregion
+
+    //        #region Depricated - 5/11/2018 Use Inline Defaulting
+    //        /*
+    //         routes.MapRoute(
+    //            name: "default", 
+    //            template: "{controller}/{action}",
+    //            defaults: new { action = "Index" });
+    //        */
+    //        #endregion
+
+    //    });
+				//*/
+				#endregion
+
         }
     }
 }
